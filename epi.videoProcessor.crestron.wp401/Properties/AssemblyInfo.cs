66a783704614ed2ef1194cc6008b2108a32dfc24
Trevor Payne <tpayne@pepperdash.com>
2023-02-07 12:02:13 -0600

    chore: added nuspec and packages.conf
    CI: updated workflows

---
+++ epi.videoProcessor.crestron.wp401/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("epi.videoProcessor.crestron.wp401")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("epi.videoProcessor.crestron.wp401")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
